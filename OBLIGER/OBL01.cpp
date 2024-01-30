77b0994726bc26b44476b4b3d46f967eae18a378
Jesper Ruud Soløst <jesper.solost@gmail.com>
2024-01-30 13:24:58 +0100

    Update OBL01.cpp

--- OBLIGER/OBL01.cpp
+++ OBLIGER/OBL01.cpp
@@ -1,5 +1,6 @@
 /**
  * Oblig nummer 1
+ * Ferdig: Nei
  * 
  * @file OBL01.cpp
  * @author Jesper Ruud Soløst
@@ -7,7 +8,76 @@
  * @date 30.01.2024
 */
 
+using namespace std;
+
 #include <iostream>
 #include <iomanip>
 #include <vector>
 #include "LesData2.h"
+
+void nyRestaurant();
+void restaurantLesData(Restaurant & restaurant);
+void restaurantSkrivData(const Restaurant* restaurant);
+void skrivAlleRestauranter();
+void skrivMeny();
+void slettAlt();
+
+struct Restaurant {
+ string navn,
+ adresse,                       // Gateadresse i en og samme by
+ type,                          // Indisk, japansk, thai, italiensk, gresk, ....
+ omtale;                        // Lengre evaluering/bekrivelse/vurdering
+ int terningkast;               // Verdien 1 - 6
+};
+
+vector <Restaurant*> gRestauranter;
+
+/**
+ * Hovedprogrammet:
+*/
+int main(){
+
+    return 0;
+}
+
+/**
+ * 
+*/
+void nyRestaurant(){
+
+}
+
+/**
+ * 
+*/
+void restaurantLesData(Restaurant & restaurant){
+
+}
+
+/**
+ * 
+*/
+void restaurantSkrivData(const Restaurant* restaurant){
+
+}
+
+/**
+ * 
+*/
+void skrivAlleRestauranter(){
+
+}
+
+/**
+ * 
+*/
+void skrivMeny(){
+
+}
+
+/**
+ * 
+*/
+void slettAlt(){
+
+}
