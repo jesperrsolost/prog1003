8e336362f60317f48baf55b0f074b83cabf14c2b
Jesper Ruud Soløst <jesper.solost@gmail.com>
2024-02-01 21:04:07 +0100

    Arbeid

--- OPPGAVER/oppg_08.cpp
+++ OPPGAVER/oppg_08.cpp
@@ -1,5 +1,5 @@
 /**
- *   Løsningsforslag til oppgave nr 8.
+ *   Lï¿½sningsforslag til oppgave nr 8.
  *
  *   Program som leser inn flere structers-data, legger disse inn i en vector,
  *   skriver vectorens totale innhold, og sletter til slutt alt i vectoren.
@@ -12,12 +12,12 @@
 #include <iostream>                //  cout, cin
 #include <string>                  //  string
 #include <vector>                  //  vector
-#include "LesData2.h"              //  Verktøykasse for lesing av diverse data
+#include "LesData2.h"              //  Verktï¿½ykasse for lesing av diverse data
 using namespace std;
 
 
 /**
- *  Kamp (med hjemme- og bortelagets navner og deres respektive scorede mål).
+ *  Kamp (med hjemme- og bortelagets navner og deres respektive scorede mï¿½l).
  */
 struct Kamp {
     string hjemmeNavn,
@@ -50,38 +50,38 @@ int main ()  {
     for (int i = 0; i < gKampene.size(); i++)  //  Skriver HELE vectorens
         kampSkrivData(gKampene[i]);            //    innhold (ALLE kampene):
 
-                            //  EN måte (av mange) å fjerne ALT i vectoren:
-    while (!gKampene.empty())  {            //  Så lenge PEKERE i vectoren:
+                            //  EN mï¿½te (av mange) ï¿½ fjerne ALT i vectoren:
+    while (!gKampene.empty())  {            //  Sï¿½ lenge PEKERE i vectoren:
         delete gKampene[gKampene.size()-1]; //  Sletter den SISTE TILPEKTE.
         gKampene.pop_back();                //  Sletter/fjerner SELVE PEKEREN.
     }
-/*                            //  En annen (og enklere?) måte:
+/*                            //  En annen (og enklere?) mï¿½te:
     for (int i = 0; i < gKampene.size(); i++)  //  Sletter ALLE kampene:
         delete gKampene[i];
     gKampene.clear();                          //  Fjerner ALT i vectoren.
 */
                                             //  Kontroll om vectorens antall:
-    cout << "\nAntall i vectoren nå: " << gKampene.size() << "\n\n";
+    cout << "\nAntall i vectoren nï¿½: " << gKampene.size() << "\n\n";
 
     return 0;
 }
 
 
 /**
- *  Leser og fyller alle en Kamp-struct sine datamedlemmer (vha ref-overfør).
+ *  Leser og fyller alle en Kamp-struct sine datamedlemmer (vha ref-overfï¿½r).
  *
- *  @param   kamp  - Structen som får fylt alle sine data (referanseoverført)
+ *  @param   kamp  - Structen som fï¿½r fylt alle sine data (referanseoverfï¿½rt)
  */
 void kampLesData(Kamp & kamp)  {
     cout << "Hjemmelag:  ";       getline(cin, kamp.hjemmeNavn);
     cout << "Bortelag:   ";       getline(cin, kamp.borteNavn);
-    kamp.hjemmeMaal = lesInt("Hjemmelagets antall mål", 0, 20);
-    kamp.borteMaal  = lesInt("Bortelagets antall mål ", 0, 20);
+    kamp.hjemmeMaal = lesInt("Hjemmelagets antall mï¿½l", 0, 20);
+    kamp.borteMaal  = lesInt("Bortelagets antall mï¿½l ", 0, 20);
 }
 
 
 /**
- *  Skriver ut på skjermen ALLE en struct sine datamedlemmer.
+ *  Skriver ut pï¿½ skjermen ALLE en struct sine datamedlemmer.
  *
  *  @param   kamp  - Structen som ALLE datamedlemmene skrives ut for
  */
--- UKEOPPGAVER/HEF08.cpp
+++ UKEOPPGAVER/HEF08.cpp
@@ -1,8 +1,78 @@
 /**
  * Oppgave 8 i heftet
  * 
+ * Ferdig: Ja
+ * 
  * @file HEF08.cpp
  * @author Jesper Ruud Soløst
  * 
  * @date 01.02.2024
 */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "LesData2.h"
+
+using namespace std;
+
+struct Kamp {
+    string hjemmeNavn,
+           borteNavn;
+    int    hjemmeMaal,
+           borteMaal;
+};
+
+void kampLesData(Kamp & kamp);
+void kampSkrivData(const Kamp* kamp);
+
+vector <Kamp*> gKampene;
+
+/**
+ * Hovedprogrammet:
+*/
+int main(){
+    char svar;
+    
+    do{
+        Kamp* nyKamp = new Kamp;
+        cout << "\nData om kamp:\n";
+        kampLesData(*nyKamp);
+        gKampene.push_back(nyKamp);
+        cout << "\n\tNy kamp lagt til har nr. " << gKampene.size() << "\n";
+
+        svar = lesChar("Lese inn data for ny kamp? (j/n): ");
+    } while (svar == 'J');
+
+    for (int i = 0; i < gKampene.size(); i++){
+        cout << "\n\tKamp nr. " << setw(2) << i + 1 << ":\n";
+        kampSkrivData(gKampene[i]);
+    }
+
+    for (int i = 0; i < gKampene.size(); i++){
+        delete gKampene[gKampene.size()-1];
+    }
+    gKampene.clear(); 
+}
+
+/**
+ * Hver kamp sine data leses inn
+ * 
+ * @param kamp - Structen som får sine datamedlemmer innlest/fylt
+*/
+void kampLesData(Kamp & kamp){
+    cout << "Hjemmelag: "; getline(cin, kamp.hjemmeNavn);
+    cout << "Bortelag : "; getline(cin, kamp.borteNavn);
+    kamp.hjemmeMaal = lesInt("Poeng til hjemmelag", 0, 100);
+    kamp.borteMaal = lesInt("Poeng til bortlag  ", 0, 100);
+}
+
+/**
+ * Skriver ut alle de lagrede kampene sine data
+ * 
+ * @param kamp - Structen som får sine datamedlemmer skrevet ut
+*/
+void kampSkrivData(const Kamp* kamp){
+    cout << "\t" << kamp->hjemmeNavn << " mot " << kamp->borteNavn << "\n"
+         << "\t" << kamp->hjemmeMaal << " - " << kamp->borteMaal << "\n";
+}
