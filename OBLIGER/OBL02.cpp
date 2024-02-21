5f70ca3847857d19dbc8acb91716cb593988b9dd
Jesper Ruud Soløst <jesper.solost@gmail.com>
2024-02-21 13:09:06 +0100

    Oblig 2

---
+++ OBLIGER/OBL02.cpp
@@ -0,0 +1,19 @@
+/**
+ * Oblig nummer 2
+ * 
+ * @file OBL02.cpp
+ * @author Jesper Ruud Soløst
+ * 
+ * @date 21.02.2024
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Hovedprogrammet:
+*/
+int main(){
+    
+}
